1ae91b6f29c8f82464003546001f561aff0ec8d8
jianyan74 <panwy@soe-soe.com>
2019-11-15 13:19:02 +0800

    初始化

---
+++ common/components/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace addons\RfMerchants\common\components;
+
+use Yii;
+use common\interfaces\AddonWidget;
+
+/**
+ * Bootstrap
+ *
+ * Class Bootstrap
+ * @package addons\RfMerchants\common\config
+ */
+class Bootstrap implements AddonWidget
+{
+    /**
+    * @param $addon
+    * @return mixed|void
+    */
+    public function run($addon)
+    {
+
+    }
+}
